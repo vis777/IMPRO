aa694c4c5053f333c19fc4792d2d45879ca2cdd2
Main-Project <vishnusnair2000@gmail.com>
2025-12-02 21:42:57 +0530

    Fix for Render production

--- IMPRO/IMPRO/urls.py
+++ IMPRO/IMPRO/urls.py
@@ -56,4 +56,6 @@ urlpatterns = [
     # path('api/employees/', views.EmployeeView.as_view(), name='employee-list'),
 
 
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
